830c6c8d5d1ce47461c6030166bd8b7de2eb69dd
Matt Stanford <mstanford@mindgrub.com>
2014-05-27 00:04:45 -0400

    Added some more functions; added some exception handling

--- controllers/domainSearchController.rb
+++ controllers/domainSearchController.rb
@@ -5,17 +5,44 @@ class MyApp < Sinatra::Application
   
   def findIconForDomain(domain)
     
-    #"controller hi: #{domain}"
-    favIconUrl = "http://#{domain}/favicon.ico"
-    
-    url = URI.parse(favIconUrl)
-    req = Net::HTTP.new(url.host, url.port)
-    res = req.request_head(url.path)
-    
-    "code for #{domain}: #{res.code}"
+    findFavIconString = findFavIcon(domain)
     
     #"favIconUrl: #{favIconUrl}"
     
+  end
+  
+  def findFavIcon(domain)
+    
+    begin
+      favIconUrl = "http://#{domain}/favicon.ico"   
+      doesExist = urlExists?(favIconUrl)
+      
+      if(doesExist)
+        return "icon exists!"
+      else
+        return "icon doesn't exist!"
+      end
+      
+    rescue URI::InvalidURIError => err
+      
+      return "invalid url!"
+      
+    end
+    
+  end
+  
+  def urlExists?(urlString)
+    
+    url = URI.parse(urlString)
+    req = Net::HTTP.new(url.host, url.port)
+    res = req.request_head(url.path)
+      
+    if res.code == 200
+      return true
+    else
+      return false
+    end
+         
   end
   
 end
